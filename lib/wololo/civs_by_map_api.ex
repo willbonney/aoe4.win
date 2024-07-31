ef73c556081b2b68d90d78226c1255e1edc3e947
William Bonney <william.bonney@beacon.works>
2024-07-31 11:14:33 -0500

    process wrs

--- lib/wololo/civs_by_map_api.ex
+++ lib/wololo/civs_by_map_api.ex
@@ -1,17 +1,66 @@
 defmodule Wololo.CivsByMapAPI do
+  require Logger
+
   @base_url Application.compile_env(:wololo, :api_base_url)
 
+  @list_of_civs [
+    "abbasid_dynasty",
+    "chinese",
+    "delhi_sultanate",
+    "english",
+    "french",
+    "holy_roman_empire",
+    "mongols",
+    "rus",
+    "ottomans",
+    "malians",
+    "byzantines",
+    "japanese",
+    "ayyubids",
+    "jeanne_darc",
+    "order_of_the_dragon",
+    "zhu_xis_legacy"
+  ]
+
   def fetch_civs_by_map() do
-    case Finch.build(:get, "#{@base_url}/stats/rm_solo/maps?include_civs=true")
-         |> Finch.request(MyFinch) do
+    Logger.info("Fetching civs_by_map data at #{DateTime.utc_now()}")
+    url = "#{@base_url}/stats/rm_solo/maps?include_civs=true"
+
+    case Finch.build(:get, url) |> Finch.request(Wololo.Finch) do
       {:ok, %Finch.Response{status: 200, body: body}} ->
-        {:ok, IO.puts("Data type: #{inspect(body)}")}
+        Logger.info("Successfully received response from API")
+        {:ok, Jason.decode!(body)}
 
       {:ok, %Finch.Response{status: status}} ->
+        Logger.error("API request failed with status: #{status}")
         {:error, "Request failed with status code: #{status}"}
 
       {:error, reason} ->
+        Logger.error("API request error: #{inspect(reason)}")
         {:error, "Request failed: #{inspect(reason)}"}
     end
+  end
+
+  def transform_data(raw_data) do
+    Enum.map(raw_data["data"], fn map_data ->
+      civ_data = map_data["civilizations"]
+
+      civ_win_rates =
+        @list_of_civs
+        |> Enum.map(fn civ ->
+          win_rate = get_in(civ_data, [civ, "win_rate"])
+          {String.to_atom(civ), format_win_rate(win_rate)}
+        end)
+        |> Enum.into(%{})
+
+      Map.merge(%{name: map_data["map"]}, civ_win_rates)
+    end)
+  end
+
+  defp format_win_rate(nil), do: "N/A"
+
+  defp format_win_rate(win_rate) do
+    :io_lib.format("~.2f%", [win_rate])
+    |> to_string()
   end
 end
--- lib/wololo_web/controllers/civs_by_map_controller.ex
+++ lib/wololo_web/controllers/civs_by_map_controller.ex
@@ -1,17 +1,19 @@
 defmodule WololoWeb.CivsByMapController do
   use WololoWeb, :controller
   alias Wololo.CivsByMapAPI
+  require Logger
 
   def index(conn, _params) do
-    render(conn, :index)
-  end
+    Logger.info("CivsByMapController.index called at #{DateTime.utc_now()}")
 
-  def show(conn, _) do
     case CivsByMapAPI.fetch_civs_by_map() do
-      {:ok, _} ->
-        render(conn, :show)
+      {:ok, civs_by_map_data} ->
+        Logger.info("Successfully fetched civs_by_map data")
+        render(conn, :index, civs_by_map_data: civs_by_map_data)
 
       {:error, reason} ->
+        Logger.error("Failed to fetch civs_by_map data: #{inspect(reason)}")
+
         conn
         |> put_flash(:error, "Failed to fetch Civs By Map: #{reason}")
         |> redirect(to: ~p"/")
