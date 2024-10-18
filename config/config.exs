1cc4958c4436d44395617e89499186297a9d6ed8
William Bonney <william.bonney@beacon.works>
2024-10-18 18:55:09 -0500

    trying with fly

--- config/config.exs
+++ config/config.exs
@@ -31,9 +31,18 @@ config :wololo, WololoWeb.Endpoint,
 config :wololo, Wololo.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
+# config :esbuild,
+#   version: "0.17.11",
+#   wololo: [
+#     args:
+#       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+#     cd: Path.expand("../assets", __DIR__),
+#     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+#   ]
+
 config :esbuild,
   version: "0.17.11",
-  wololo: [
+  default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
