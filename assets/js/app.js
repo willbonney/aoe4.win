0f5460659367c5230dad20c1240ae1e0ef7d6b67
William Bonney <william.bonney@beacon.works>
2024-10-31 17:47:47 -0500

    improve error handling and timeouts in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -281,17 +281,22 @@ const csrfToken = document
 	.getAttribute("content");
 
 const liveSocket = new LiveSocket("/live", Socket, {
-	longPollFallbackMs: 2500,
+	longPollFallbackMs: 5000,
 	params: { _csrf_token: csrfToken },
 	hooks: hooks,
 });
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+topbar.config({ barColors: { 0: "#273649" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
 window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
+liveSocket.socket.onError((error) => {
+	console.error("LiveView socket error:", error);
+	// Optionally attempt reconnect
+	setTimeout(() => liveSocket.connect(), 1000);
+  });
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
