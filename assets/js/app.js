0dcd9f0b1261a9e18a6344b7ff52f0986c6a86a3
willbonney <hello@wbonney.com>
2025-07-24 23:47:12 -0500

    Use type: "module" in package.json, rename topbar with cjs extension and alter imports in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
-import topbar from "../vendor/topbar";
+import * as topbar from "../vendor/topbar.cjs";
 
 // *****
 // *****
@@ -28,7 +28,7 @@ import topbar from "../vendor/topbar";
 
 // from https://medium.com/@lionel.aimerie/integrating-chart-js-into-elixir-phoenix-for-visual-impact-9a3991f0690f
 
-import {Chart} from "chart.js";
+import {Chart} from "chart.js/auto";
 
 const MUI_COLORS = [
   "rgba(255, 193, 7, 1)", // #FFC107
