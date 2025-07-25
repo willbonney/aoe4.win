827c45671e3f2bf4c4a8243085097864a202df2f
willbonney <hello@wbonney.com>
2025-07-24 22:25:16 -0500

    Use new import style for chartjs?

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,8 +28,7 @@ import topbar from "../vendor/topbar";
 
 // from https://medium.com/@lionel.aimerie/integrating-chart-js-into-elixir-phoenix-for-visual-impact-9a3991f0690f
 
-import Chart from "chart.js/auto";
-// import { easingEffects } from "chart.js/helpers";
+import {Chart} from "chart.js";
 
 const MUI_COLORS = [
   "rgba(255, 193, 7, 1)", // #FFC107
